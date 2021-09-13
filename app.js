06fd2d93f03a8363a5334e06b92df4c341ccd803
Ankita Sahu <sahuankitaofc.1@gmail.com>
2021-09-13 14:14:07 +0530

    final update

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption=document.querySelector('.filter-todo');
+
+
 //Event Listeners
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click',filterTodo);
+
 
 //Functions
 function addTodo(event){
@@ -35,9 +40,9 @@ function addTodo(event){
 }
 
 function deleteCheck(e){
-    const item=e.target;
-    //delete Todo from
-    if(item.classList[0]==='trash-btn'){
+    const item = e.target;
+    //delete Todo 
+    if (item.classList[0] === 'trash-btn'){
         const todo=item.parentElement;
         //Animation
         todo.classList.add("fall");
@@ -47,6 +52,35 @@ function deleteCheck(e){
    }
     //Check Mark
     if(item.classList[0]==="complete-btn"){
-        const todo=item.parentElement;todo.classList.toggle("completed");
+        const todo=item.parentElement;
+        todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e){
+    const todos=todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display="flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display="flex";
+                }    
+                else{
+                    todo.style.display="none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display="flex";
+                } 
+                else{
+                    todo.style.display="none";
+                }   
+              
+                break;
+        }
+    });
 }
